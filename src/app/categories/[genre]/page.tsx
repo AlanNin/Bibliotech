24e8250cfdc3a814162b666fd4ed75ff6a73a876
BuildTools <unconfigured@null.spigotmc.org>
2024-06-26 19:48:12 -0400

    details

--- src/app/categories/[genre]/page.tsx
+++ src/app/categories/[genre]/page.tsx
@@ -6,6 +6,7 @@ import { BookCard } from "~/app/_shared/book_card";
 import ReactLoading from "react-loading";
 
 import { useParams } from "next/navigation";
+import { FaceFrownIcon } from "@heroicons/react/24/outline";
 
 export default function Home() {
   const [books, setBooks] = useState<any>();
@@ -39,19 +40,34 @@ export default function Home() {
           <ReactLoading type="spin" color="#2c94dd" height={70} width={70} />
         </div>
       ) : (
-        <main className={styles.main}>
-          <div className={styles.container}>
-            <h1 className={styles.categoryTitle}>
-              Category:{" "}
-              <span className={styles.categoryTitleSpan}>{formattedTitle}</span>
-            </h1>
-            <div className={styles.booksWrapper}>
-              {books?.map((book: any) => (
-                <BookCard key={book.key} book={book} />
-              ))}
-            </div>
-          </div>
-        </main>
+        <>
+          {books?.length > 0 ? (
+            <main className={styles.main}>
+              <div className={styles.container}>
+                <h1 className={styles.categoryTitle}>
+                  Books related to{" "}
+                  <span className={styles.categoryTitleSpan}>
+                    {formattedTitle}
+                  </span>
+                </h1>
+                <div className={styles.booksWrapper}>
+                  {books?.map((book: any) => (
+                    <BookCard key={book.key} book={book} />
+                  ))}
+                </div>
+              </div>
+            </main>
+          ) : (
+            <main className={styles.main}>
+              <div className={styles.containerNotFound}>
+                <h1 className={styles.title}>
+                  No results found for this query
+                </h1>
+                <FaceFrownIcon className={styles.icon} />
+              </div>
+            </main>
+          )}
+        </>
       )}
     </>
   );
